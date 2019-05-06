d903ac190b56a9d9956d6db3b994e2d29783a398
alyssa-hatfield <47366310+xCthaeh@users.noreply.github.com>
2019-05-06 17:40:06 -0400

    navbar and subnav bar completed

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import SubNav from "./components/SubNav";
+import data from "./data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    data: []
+  };
+
+  componentDidMount() {
+    this.setState({
+      data: data
+    });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="navbar-wrapper">
+          <NavBar data={this.state.data} />
+        </div>
+        <div className="subnav-wrapper">
+          {this.state.data.map(link => (
+            <Route
+              key={link.id}
+              path={`/${link.name.toLowerCase()}`}
+              render={props => <SubNav {...props} link={link} />}
+            />
+          ))}
+        </div>
+        <div className="fancyfooter">
+          <p>Fancy Apple Footer Here</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
